905c9f5fb01ea067da253ce50667cc1a58dbab83
codnjs779 <codnjs779@naver.com>
2022-01-17 17:46:00 +0900

    google login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import "./App.module.css";
+import Login from "./components/Login/Login";
 
 function App() {
-    return <div className="App">helllo</div>;
+    return (
+        <div className="g-log">
+            <Login />
+        </div>
+    );
 }
 
 export default App;
