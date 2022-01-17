293c328dbfb88929b85489b77320d94cf7790e48
codnjs779 <codnjs779@naver.com>
2022-01-17 11:54:58 +0900

    setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-     helllo
-    </div>
-  );
+    return <div className="App">helllo</div>;
 }
 
 export default App;
