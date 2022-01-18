9298c5e330c34c7aad59ec5a4040350bff294dca
codnjs779 <codnjs779@naver.com>
2022-01-19 02:13:00 +0900

    상단바

--- src/components/TemplateAll/Template.jsx
+++ src/components/TemplateAll/Template.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import CreateCardTemplate from "../create_card/CreateCardTemplate";
-
+import logo from "../../images/logo.png";
+import styles from "../templateAll/Template.module.css";
+import LogoutBtn from "../logout/LogOutBtn";
 function Template(props) {
     return (
-        <div>
+        <>
+            <div className={styles.titleBox}>
+                <LogoutBtn></LogoutBtn>
+                <div className={styles.contents}>
+                    <div>
+                        <img src={logo} alt=""></img>
+                    </div>
+                    <h1>Business Card Maker</h1>
+                </div>
+            </div>
+
             <CreateCardTemplate />
-        </div>
+        </>
     );
 }
-
 export default Template;
