68dd39870dff342b95a4d7cc417c042598e6a407
codnjs779 <codnjs779@naver.com>
2022-01-20 02:38:06 +0900

    버튼 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.module.css";
+import { db } from "./config/firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 //Router
 import { Routes, Route, Link } from "react-router-dom";
 import Login from "./components/loginBox/Login";
 import Template from "./components/templateAll/Template";
+import { useEffect } from "react";
 
 function App() {
+    const usersCollectionRef = collection(db, "users");
+    useEffect(() => {
+        const getUsers = async () => {
+            const data = await getDocs(usersCollectionRef);
+            console.log(data);
+        };
+        getUsers();
+    }, []);
     return (
         <Routes>
             <Route path="/" element={<Login />} />
